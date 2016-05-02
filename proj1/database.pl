e014a5109f292e7eca29f2204bdd6114e04f1b6b
Diogo Marques <xmarques999@hotmail.com>
2016-05-02 20:30:20 +0100

    projeto #8: database

---
+++ proj1/database.pl
@@ -0,0 +1,98 @@
+autor(cacb, "Camilo", "Castelo-Branco", 1825, 1890, por, []).
+autor(juld, "Júlio", "Diis", 1839, 1871, por, []).
+autor(qroz, "Eça", "Queiróz", 1845, 1900, por, []).
+autor(verf, "Vergílo", "Ferreira", 1916, 1996, por, []).
+autor(lcam, "Luís", "Camões", 1524, 1580, por, []).
+
+livro(cacb-pund, 'Os Pundonores Desagravados', cacb, 1845, poesia, null).
+livro(cacb-aceu, 'Agostinho de Ceuta', cacb, 1847, teatro, null).
+livro(cacb-insp, 'Inspirações', cacb, 1851, poesia, null).
+livro(cacb-mis1, 'Mistérios de Lisboa - I', 1854, romance, cacb-misl).
+livro(cacb-mis2, 'Mistérios de Lisboa - II', 1854, romance, cacb-misl).
+livro(cacb-mis3, 'Mistérios de Lisboa - II', 1854, romance, cacb-misl).
+livro(cacb-esfu, 'Estrelas Funestas', cacb, 1862, romance, null).
+livro(cacb-espr, 'Estrelas Propícias', cacb, 1863, romance, null).
+livro(cacb-aper, 'Amor de Perdição', cacb, 1862, romance, null).
+livro(cacb-asal, 'Amor de Salvação', cacb, 1864, romance, null).
+livro(cacb-lnp1, 'Livro Negro de Padre Dinis - I', cacb, 1855, naosei, cacb-lnpd).
+livro(cacb-lnp2, 'Livro Negro de Padre Dinis - II', cacb, 185, naosei, cacb-lnpd).
+livro(cacb-labe, 'Lágrimas Abençoadas', cacb, 1857, romance, null).
+
+livro(juld-pupr, 'As Pupilas do Senhor Reitor', juld, 1867, romance, null).
+livro(juld-fing, 'Uma Família Inglesa', juld, 1868, romance, null).
+livro(juld-morg, 'A Morgadinha dos Canaviais', juld, 1868, romance, null).
+livro(juld-casm, 'Os Fidalgos da Casa Mourisca', juld, 1871, romance, null).
+livro(juld-poes, 'Poesias', juld, 1873, poesia, null).
+
+livro(qroz-pama, 'O Crime do Padro Amaro', qroz, 1875, romance, null).
+livro(qroz-mand, 'O Mandarim', qroz, 1880, novela, null).
+livro(qroz-pbas, 'O Primo Basílio', qroz, 1878, romance, null).
+livro(qroz-reli, 'A Relíquia', qroz, 1887, romance, null).
+livro(qroz-maia, 'Os Maias', qroz, 1888, romance, null).
+livro(qroz-cram, 'A Ilustre Casa de Ramires', 1900, romance, null).
+
+livro(verf-muda, 'Mudança', verf, 1949, null, null).
+livro(verf-msub, 'Manhã Submersa', verf, 1953, null, null).
+livro(verf-apar, 'Aparição', verf, 1959, null, null).
+livro(verf-epol, 'Estrela Polar', verf, 1960, null, null).
+
+livro(lcam-lusi, 'Lusíadas', lcam, 1572, poesia, null).
+livro(lcam-afil, 'Auto de Filodemo', lcam, 1587, teatro, null).
+livro(lcam-anfi, 'Anfitriões', lcam, 1587, teatro, null).
+
+autor(mcou, "Mia", "Couto", 1955, -1, moz, []).
+
+livro(mcou-vnoi, "Vozes Anoitecidas", 1986, conto, null).
+livro(mcou-esto, "Estórias Abensonhadas", 1994, conto, null).
+livro(mcou-cron, "Cronicando", 1988, cronica, null).
+livro(mcou-tson, "Terra Sonâmbula", 1992, romance, null).
+livro(mcou-mamq, "Mar Me Quer", 1998, romance, null).
+livro(mcou-freq, "Pensageiro Frequente", 2010, romance, null).
+
+autor(cbro, "Charlotte", "Brontë", 1816, 1855, gbr, []).
+autor(ebro, "Emily", "Brontë", 1818, 1848, gbr, []).
+
+livro(cbro-jeyr, 'Jane Eyre', cbro, 1847, romance, null).
+livro(ebro-wuth, 'Wuthering Heights', ebro, 1847, romance, null).
+
+autor(mtwn, "Mark", "Twain", 1835, 1910, usa, []).
+autor(owld, "Oscar", "Wilde", 1864, 1900, ire, []).
+autor(ehem, "Ernest", "Hemingway", 1899, 1961, usa, []).
+autor(ebla, "Eric", "Blair", 1903, 1950, ind, [gowr]).
+
+pseudo(gowr, "George", "Orwell").
+
+livro(ehem-bell, 'For Whom the Bell Tolls', ehem, 1940, null, null).
+livro(ehem-gran, 'Fifty Grand', ehem, 1927, null, null).
+livro(ehem-icam, 'Indian Camp', ehem, 1926, null, null).
+livro(ehem-fare, 'A Farewell to Arms', ehem, 1929, null, null).
+
+autor(isal, "Isabel", "Allende", 1942, -1, chi, []).
+autor(gagm, "Gabriel", "Garcia Márquez", 1927, 2014, col, []).
+
+livro(gagm-sole, "Cem Anos de Solidão", gagm, 1967, romance, null).
+livro(gagm-oton, "O Outono do Patriarca", gagm, 1975, romance, null).
+livro(gagm-amor, "O Amor nos Tempos de Cólera", gagm, 1985, romance, null).
+
+livro(isal-cesp, "A Casa dos Espíritos", isal, 1982, romance, null).
+
+% pais(id, nome, idioma_oficial, gentilico_m, gentilico_f)
+pais(ang, "Angola", lo-pt, "angolano", "angolana").
+pais(aus, "Austrália", lingua-en, "australiano", "australiana").
+pais(aut, "Áustria", lingua-de, "austríaco", "austríaca").
+pais(bel, "Bélgica", frances, "belga", "belga").
+pais(bra, "Brasil", portugues, "brasileiro", "brasileira").
+pais(col, "Colômbia", espanhol, "colombiano", "colombiana").
+pais(esp, "Espanha", espanhol, "espanhol", "espanhola").
+pais(fin, "Finlândia", finlandes, "finlandês", "finlandesa").
+pais(fra, "França", frances, "francês", "francesa").
+pais(gbr, "Reino Unido", ingles, "inglês", "inglesa").
+pais(ger, "Alemanha",  alemao, "alemão", "alemã").
+pais(ire, "Irlanda", ingles, "irlandês", "irlandesa").
+pais(moz, "Moçambique", portugues, "moçambicano", "moçambicana").
+pais(nor, "Noruega", noruegues, "norueguês", "norueguesa").
+pais(por, "Portugal", portugues, "português", "portuguesa").
+
+lingua_oficial(portugues, "português", lo-pt).
+lingua_oficial(aus, lo-en).
+lingua_oficial(aut, lo-de).
