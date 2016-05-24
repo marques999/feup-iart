a4ec5baa4609f369dcfc8b6f027e17c5506abe28
Mellus <dorianMahlerian@gmail.com>
2016-05-24 10:22:44 +0100

    ASCII ....... -.-

--- proj1/server/lexico.pl
+++ proj1/server/lexico.pl
@@ -1,3 +1,5 @@
+% consult(['~/Desktop/feup-iart-master/proj1/server/lexico.pl']).
+
 numero_ordinal(s-m, 1) --> [primeiro].
 numero_ordinal(s-f, 1) --> [primeira].
 numero_ordinal(p-m, 1) --> [primeiros].
@@ -27,25 +29,24 @@ numero_ordinal(p-f, -2) --> [penultimas].
 % Determinantes Artigos                                   %
 %---------------------------------------------------------%
 
-artigo_definido(s-m) --> [o].
-artigo_definido(s-f) --> [a].
-artigo_definido(p-m) --> [os].
-artigo_definido(p-f) --> [as].
+art_def(s-m) --> [o].
+art_def(s-f) --> [a].
+art_def(p-m) --> [os].
+art_def(p-f) --> [as].
 
-artigo_indefinido(s-m) --> [um].
-artigo_indefinido(s-f) --> [uma].
-artigo_indefinido(p-m) --> [uns].
-artigo_indefinido(p-f) --> [umas].
+art_indef(s-m) --> [um].
+art_indef(s-f) --> [uma].
+art_indef(p-m) --> [uns].
+art_indef(p-f) --> [umas].
 
 %---------------------------------------------------------%
 % Preposições                                             %
 %---------------------------------------------------------%
 
 preposicao(s-m) --> [ao].
-preposicao(s-f) --> [à].
+% preposicao(s-f) --> [à].
 preposicao(p-m) --> [aos].
-preposicao(p-f) --> [às].
-
+% preposicao(p-f) --> [às].
 preposicao(_-_) --> [de].
 preposicao(s-m) --> [do].
 preposicao(s-f) --> [da].
@@ -65,15 +66,15 @@ pronome_pessoal(p-f) --> [suas].
 % Pronomes Interrogativos                                 %
 %---------------------------------------------------------%
 
-pronome_interrogativo(_-_, qualitativo) --> [que].
-pronome_interrogativo(_-_, qualitativo) --> [quem].
-pronome_interrogativo(_-_, qualitativo) --> [quando].
-pronome_interrogativo(s-_, qualitativo) --> [quais].
-pronome_interrogativo(p-_, qualitativo) --> [quais].
-pronome_interrogativo(p-m, quantitativo) --> [quantos].
-pronome_interrogativo(p-f, quantitativo) --> [quantas].
+pron_int(s-_, ql) --> [quais].
+pron_int(p-_, ql) --> [quais].
+pron_int(_-_, ql) --> [que].
+pron_int(_-_, ql) --> [quem].
+pron_int(_-_, ql) --> [quando].
+pron_int(p-m, qn) --> [quantos].
+pron_int(p-f, qn) --> [quantas].
 
-pronome_relativo(_-_) --> [que].
+pron_rel(_-_) --> [que].
 
 adjetivo(s-_, recente) --> [recente].
 adjetivo(p-_, recente) --> [recentes].
@@ -83,8 +84,11 @@ adjetivo(s-f, antigo) --> [antiga].
 adjetivo(p-m, antigo) --> [antigos].
 adjetivo(p-f, antigo) --> [antigas].
 
-adjetivo(_) --> [mais].
-adjetivo(_) --> [menos].
+adjetivo_super(mais) --> [mais].
+adjetivo_super(menos) --> [menos].
+
+adj_comp(mais,antigo, maisantigo).
+adj_comp(_,Adj,Adj).
 
 adverbio(inclusao) --> [ainda].
 adverbio(inclusao) --> [ate].
@@ -109,188 +113,186 @@ adverbio(p-f, pouco) --> [poucas].
 % Substantivos: Autores                                   %
 %---------------------------------------------------------%
 
-substantivo(s-m, autor) --> [autor].
-substantivo(s-f, autor) --> [autora].
-substantivo(p-m, autor) --> [autores].
-substantivo(p-f, autor) --> [autoras].
+subst(s-m, autor) --> [autor].
+subst(s-f, autor) --> [autora].
+subst(p-m, autor) --> [autores].
+subst(p-f, autor) --> [autoras].
 
-substantivo(s-m, autor) --> [escritor].
-substantivo(s-f, autor) --> [escritora].
-substantivo(p-m, autor) --> [escritores].
-substantivo(p-f, autor) --> [escritoras].
+subst(s-m, autor) --> [escritor].
+subst(s-f, autor) --> [escritora].
+subst(p-m, autor) --> [escritores].
+subst(p-f, autor) --> [escritoras].
 
 %---------------------------------------------------------%
 % Substantivos: Colecções                                 %
 %---------------------------------------------------------%
 
-substantivo(s-m, colecao) --> [colecao].
-substantivo(p-m, colecao) --> [colecoes].
-substantivo(s-f, colecao) --> [saga].
-substantivo(p-f, colecao) --> [sagas].
-substantivo(s-f, colecao) --> [trilogia].
-substantivo(p-f, colecao) --> [trilogias].
-substantivo(s-m, colecao) --> [volume].
-substantivo(p-m, colecao) --> [volumes].
+subst(s-m, colecao) --> [colecao].
+subst(p-m, colecao) --> [colecoes].
+subst(s-f, colecao) --> [saga].
+subst(p-f, colecao) --> [sagas].
+subst(s-f, colecao) --> [trilogia].
+subst(p-f, colecao) --> [trilogias].
+subst(s-m, colecao) --> [volume].
+subst(p-m, colecao) --> [volumes].
 
 %---------------------------------------------------------%
 % Substantivos: Pseudónimos                               %
 %---------------------------------------------------------%
 
-substantivo(s-_, pseudonimo) --> [criptónimos].
-substantivo(p-_, pseudonimo) --> [criptónimoss].
-substantivo(s-_, pseudonimo) --> [heterónimo].
-substantivo(p-_, pseudonimo) --> [heterónimos].
-substantivo(s-_, pseudonimo) --> [ortónimo].
-substantivo(p-_, pseudonimo) --> [ortónimos].
-substantivo(s-_, pseudonimo) --> [pseudónimo].
-substantivo(p-_, pseudonimo) --> [pseudónimos].
+% subst(s-_, pseudonimo) --> [criptónimos].
+% subst(p-_, pseudonimo) --> [criptónimoss].
+% subst(s-_, pseudonimo) --> [heterónimo].
+% subst(p-_, pseudonimo) --> [heterónimos].
+% subst(s-_, pseudonimo) --> [ortónimo].
+% subst(p-_, pseudonimo) --> [ortónimos].
+% subst(s-_, pseudonimo) --> [pseudónimo].
+% subst(p-_, pseudonimo) --> [pseudónimos].
 
-substantivo(s-_, morte) --> [morte].
-substantivo(s-_, morte) --> [falecimento].
-substantivo(s-_, nascimento) --> [nascimento].
+subst(s-_, morte) --> [morte].
+subst(s-_, morte) --> [falecimento].
+subst(s-_, nascimento) --> [nascimento].
 
 adverbio(s, antes) --> [antes].
 adverbio(s, depois) --> [depois].
-
 adverbio(s, anterior) --> [anterior].
 adverbio(p, anterior) --> [anteriores].
-
 adverbio(s, posterior) --> [posterior].
 adverbio(p, posterior) --> [posteriores].
-adverbio(s, posterior) --> [após].
+% adverbio(s, posterior) --> [após].
 
-substantivo(s-m, ano) --> [ano].
-substantivo(p-m, ano) --> [anos].
+subst(s-m, ano) --> [ano].
+subst(p-m, ano) --> [anos].
 
-substantivo(s-m, seculo) --> [seculo].
-substantivo(p-m, seculo) --> [seculos].
+subst(s-m, seculo) --> [seculo].
+subst(p-m, seculo) --> [seculos].
 
-substantivo(s-m, idioma) --> [idioma].
-substantivo(p-m, idioma) --> [idiomas].
-substantivo(s-f, idioma) --> [lingua].
-substantivo(p-f, idioma) --> [linguas].
+subst(s-m, idioma) --> [idioma].
+subst(p-m, idioma) --> [idiomas].
+subst(s-f, idioma) --> [lingua].
+subst(p-f, idioma) --> [linguas].
 
-substantivo(s-f, nacionalidade) --> [nacionalidade].
-substantivo(p-f, nacionalidade) --> [nacionalidades].
+subst(s-f, nacionalidade) --> [nacionalidade].
+subst(p-f, nacionalidade) --> [nacionalidades].
 
-substantivo(s-f, idade) --> [idade].
+subst(s-f, idade) --> [idade].
 substantivo(p-f, idade) --> [idades].
 
 %---------------------------------------------------------%
 % Substantivos: Géneros                                   %
 %---------------------------------------------------------%
 
-substantivo_genero(s-m, _) --> [livro].
-substantivo_genero(p-m, _) --> [livros].
+subst_gen(_,s-m) --> [livro].
+subst_gen(_,p-m) --> [livros].
 
-substantivo_genero(s-f, _) --> [obra].
-substantivo_genero(p-f, _) --> [obras].
+subst_gen(_,s-f) --> [obra].
+subst_gen(_,p-f) --> [obras].
 
-substantivo_genero(comedia, s-f) --> [comedia].
-substantivo_genero(comedia, p-f) --> [comedias].
+subst_gen(comedia, s-f) --> [comedia].
+subst_gen(comedia, p-f) --> [comedias].
 
-substantivo_genero(cronica, s-f) --> [cronica].
-substantivo_genero(cronica, p-f) --> [cronicas].
+subst_gen(cronica, s-f) --> [cronica].
+subst_gen(cronica, p-f) --> [cronicas].
 
-substantivo_genero(conto, s-m) --> [conto].
-substantivo_genero(conto, p-m) --> [contos].
+subst_gen(conto, s-m) --> [conto].
+subst_gen(conto, p-m) --> [contos].
 
-substantivo_genero(drama, s-m) --> [drama].
-substantivo_genero(drama, p-m) --> [dramas].
+subst_gen(drama, s-m) --> [drama].
+subst_gen(drama, p-m) --> [dramas].
 
-substantivo_genero(manga, s-m) --> [manga].
-substantivo_genero(manga, p-m) --> [mangas].
+subst_gen(manga, s-m) --> [manga].
+subst_gen(manga, p-m) --> [mangas].
 
-substantivo_genero(poesia, s-f) --> [poesia].
-substantivo_genero(poesia, p-f) --> [poesias].
+subst_gen(poesia, s-f) --> [poesia].
+subst_gen(poesia, p-f) --> [poesias].
 
-substantivo_genero(prosa, s-f) --> [prosa].
-substantivo_genero(prosa, p-f) --> [prosa].
+subst_gen(prosa, s-f) --> [prosa].
+subst_gen(prosa, p-f) --> [prosa].
 
-substantivo_genero(romance, s-m) --> [romance].
-substantivo_genero(romance, p-m) --> [romances].
+subst_gen(romance, s-m) --> [romance].
+subst_gen(romance, p-m) --> [romances].
 
-substantivo_genero(teatro, s-m) --> [teatro].
-substantivo_genero(teatro, p-m) --> [teatros].
+subst_gen(teatro, s-m) --> [teatro].
+subst_gen(teatro, p-m) --> [teatros].
 
-substantivo_genero(tragedia, s-f) --> [tragedia].
-substantivo_genero(tragedia, p-f) --> [tragedias].
+subst_gen(tragedia, s-f) --> [tragedia].
+subst_gen(tragedia, p-f) --> [tragedias].
 
 %---------------------------------------------------------%
 % Substantivos: Nacionalidades                            %
 %---------------------------------------------------------%
 
-substantivo_nacionalidade(ger, s-m) --> [alemao].
-substantivo_nacionalidade(ger, s-f) --> [alema].
-substantivo_nacionalidade(ger, p-m) --> [alemaes].
-substantivo_nacionalidade(ger, p-f) --> [alemas].
+subst_nac(ger, s-m) --> [alemao].
+subst_nac(ger, s-f) --> [alema].
+subst_nac(ger, p-m) --> [alemaes].
+subst_nac(ger, p-f) --> [alemas].
 
-substantivo_nacionalidade(chi, s-m) --> [chileno].
-substantivo_nacionalidade(chi, s-f) --> [chilena].
-substantivo_nacionalidade(chi, p-m) --> [chilenos].
-substantivo_nacionalidade(chi, p-f) --> [chilenas].
+subst_nac(chi, s-m) --> [chileno].
+subst_nac(chi, s-f) --> [chilena].
+subst_nac(chi, p-m) --> [chilenos].
+subst_nac(chi, p-f) --> [chilenas].
 
-substantivo_nacionalidade(col, s-m) --> [colombiano].
-substantivo_nacionalidade(col, s-f) --> [colombiana].
-substantivo_nacionalidade(col, p-m) --> [colombianos].
-substantivo_nacionalidade(col, p-f) --> [colombianas].
+subst_nac(col, s-m) --> [colombiano].
+subst_nac(col, s-f) --> [colombiana].
+subst_nac(col, p-m) --> [colombianos].
+subst_nac(col, p-f) --> [colombianas].
 
-substantivo_nacionalidade(esp, s-m) --> [espanhol].
-substantivo_nacionalidade(esp, s-f) --> [espanhola].
-substantivo_nacionalidade(esp, p-m) --> [espanhois].
-substantivo_nacionalidade(esp, p-f) --> [espanholas].
+subst_nac(esp, s-m) --> [espanhol].
+subst_nac(esp, s-f) --> [espanhola].
+subst_nac(esp, p-m) --> [espanhois].
+subst_nac(esp, p-f) --> [espanholas].
 
-substantivo_nacionalidade(fra, s-m) --> [frances].
-substantivo_nacionalidade(fra, s-f) --> [francesa].
-substantivo_nacionalidade(fra, p-m) --> [franceses].
-substantivo_nacionalidade(fra, p-f) --> [francesas].
+subst_nac(fra, s-m) --> [frances].
+subst_nac(fra, s-f) --> [francesa].
+subst_nac(fra, p-m) --> [franceses].
+subst_nac(fra, p-f) --> [francesas].
 
-substantivo_nacionalidade(gbr, s-m) --> [ingles].
-substantivo_nacionalidade(gbr, s-f) --> [inglesa].
-substantivo_nacionalidade(gbr, p-m) --> [ingleses].
-substantivo_nacionalidade(gbr, p-f) --> [inglesas].
+subst_nac(gbr, s-m) --> [ingles].
+subst_nac(gbr, s-f) --> [inglesa].
+subst_nac(gbr, p-m) --> [ingleses].
+subst_nac(gbr, p-f) --> [inglesas].
 
-substantivo_nacionalidade(ind, s-m) --> [indiano].
-substantivo_nacionalidade(ind, s-f) --> [indiana].
-substantivo_nacionalidade(ind, p-m) --> [indianos].
-substantivo_nacionalidade(ind, p-f) --> [indianas].
+subst_nac(ind, s-m) --> [indiano].
+subst_nac(ind, s-f) --> [indiana].
+subst_nac(ind, p-m) --> [indianos].
+subst_nac(ind, p-f) --> [indianas].
 
-substantivo_nacionalidade(ire, s-m) --> [irlandes].
-substantivo_nacionalidade(ire, s-f) --> [irlandesa].
-substantivo_nacionalidade(ire, p-m) --> [irlandeses].
-substantivo_nacionalidade(ire, p-f) --> [irlandesas].
+subst_nac(ire, s-m) --> [irlandes].
+subst_nac(ire, s-f) --> [irlandesa].
+subst_nac(ire, p-m) --> [irlandeses].
+subst_nac(ire, p-f) --> [irlandesas].
 
-substantivo_nacionalidade(jpn, s-m) --> [japones].
-substantivo_nacionalidade(jpn, s-f) --> [japonesa].
-substantivo_nacionalidade(jpn, p-m) --> [japoneses].
-substantivo_nacionalidade(jpn, p-f) --> [japonesas].
+subst_nac(jpn, s-m) --> [japones].
+subst_nac(jpn, s-f) --> [japonesa].
+subst_nac(jpn, p-m) --> [japoneses].
+subst_nac(jpn, p-f) --> [japonesas].
 
-substantivo_nacionalidade(moz, s-m) --> [mocambicano].
-substantivo_nacionalidade(moz, s-f) --> [mocambicana].
-substantivo_nacionalidade(moz, p-m) --> [mocambicanos].
-substantivo_nacionalidade(moz, p-f) --> [mocambicanas].
+subst_nac(moz, s-m) --> [mocambicano].
+subst_nac(moz, s-f) --> [mocambicana].
+subst_nac(moz, p-m) --> [mocambicanos].
+subst_nac(moz, p-f) --> [mocambicanas].
 
-substantivo_nacionalidade(por, s-m) --> [portugues].
-substantivo_nacionalidade(por, s-f) --> [portuguesa].
-substantivo_nacionalidade(por, p-m) --> [portugueses].
-substantivo_nacionalidade(por, p-f) --> [portuguesas].
+subst_nac(por, s-m) --> [portugues].
+subst_nac(por, s-f) --> [portuguesa].
+subst_nac(por, p-m) --> [portugueses].
+subst_nac(por, p-f) --> [portuguesas].
 
-substantivo_nacionalidade(rus, s-m) --> [russo].
-substantivo_nacionalidade(rus, s-f) --> [russa].
-substantivo_nacionalidade(rus, p-m) --> [russos].
-substantivo_nacionalidade(rus, p-f) --> [russas].
+subst_nac(rus, s-m) --> [russo].
+subst_nac(rus, s-f) --> [russa].
+subst_nac(rus, p-m) --> [russos].
+subst_nac(rus, p-f) --> [russas].
 
-substantivo_nacionalidade(usa, s-m) --> [americano].
-substantivo_nacionalidade(usa, s-f) --> [americana].
-substantivo_nacionalidade(usa, p-m) --> [americanos].
-substantivo_nacionalidade(usa, p-f) --> [americanas].
+subst_nac(usa, s-m) --> [americano].
+subst_nac(usa, s-f) --> [americana].
+subst_nac(usa, p-m) --> [americanos].
+subst_nac(usa, p-f) --> [americanas].
 
 %---------------------------------------------------------%
 % Substantivos: Idiomas                                   %
 %---------------------------------------------------------%
 
 substantivo_idioma(alemao, s-m) --> [alemao].
-substantivo_idioma(alemao, s-f) --> [alemã].
+substantivo_idioma(alemao, s-f) --> [alema].
 
 substantivo_idioma(espanhol, s-m) --> [espahol].
 substantivo_idioma(espanhol, s-f) --> [espanhola].
@@ -311,37 +313,37 @@ substantivo_idioma(russo, s-f) --> [russa].
 % Substantivos: Continentes                               %
 %---------------------------------------------------------%
 
-substantivo_continente(s-m, africa) --> [africano].
-substantivo_continente(s-f, africa) --> [africana].
-substantivo_continente(p-m, africa) --> [africanos].
-substantivo_continente(p-f, africa) --> [africanas].
+subst_cont(s-m, africa) --> [africano].
+subst_cont(s-f, africa) --> [africana].
+subst_cont(p-m, africa) --> [africanos].
+subst_cont(p-f, africa) --> [africanas].
 
-substantivo_continente(s-m, asia) --> [asiatico].
-substantivo_continente(s-f, asia) --> [asiatica].
-substantivo_continente(p-m, asia) --> [asiaticos].
-substantivo_continente(p-f, asia) --> [asiaticas].
+subst_cont(s-m, asia) --> [asiatico].
+subst_cont(s-f, asia) --> [asiatica].
+subst_cont(p-m, asia) --> [asiaticos].
+subst_cont(p-f, asia) --> [asiaticas].
 
-substantivo_continente(s-m, europa) --> [europeu].
-substantivo_continente(s-f, europa) --> [europeia].
-substantivo_continente(p-m, europa) --> [europeus].
-substantivo_continente(p-f, europa) --> [europeias].
+subst_cont(s-m, europa) --> [europeu].
+subst_cont(s-f, europa) --> [europeia].
+subst_cont(p-m, europa) --> [europeus].
+subst_cont(p-f, europa) --> [europeias].
 
-substantivo_continente(s-m, america_norte) --> [norte-americano].
-substantivo_continente(s-f, america_norte) --> [norte-americana].
-substantivo_continente(p-m, america_norte) --> [norte-americanos].
-substantivo_continente(p-f, america_norte) --> [norte-americanas].
+subst_cont(s-m, america_norte) --> [norte-americano].
+subst_cont(s-f, america_norte) --> [norte-americana].
+subst_cont(p-m, america_norte) --> [norte-americanos].
+subst_cont(p-f, america_norte) --> [norte-americanas].
 
-substantivo_continente(s-m, america_sul) --> [sul-americano].
-substantivo_continente(s-f, america_sul) --> [sul-americana].
-substantivo_continente(p-m, america_sul) --> [sul-americanos].
-substantivo_continente(p-f, america_sul) --> [sul-americanas].
+subst_cont(s-m, america_sul) --> [sul-americano].
+subst_cont(s-f, america_sul) --> [sul-americana].
+subst_cont(p-m, america_sul) --> [sul-americanos].
+subst_cont(p-f, america_sul) --> [sul-americanas].
 
 %---------------------------------------------------------%
 % Verbos: Presente                                        %
 %---------------------------------------------------------%
 
-verbo(s, ser, presente) --> [é].
-verbo(p, ser, presente) --> [são].
+% verbo(s, ser, presente) --> [é].
+% verbo(p, ser, presente) --> [são].
 
 verbo(s, encontrar, presente) --> [encontra].
 verbo(p, encontrar, presente) --> [encontram].
